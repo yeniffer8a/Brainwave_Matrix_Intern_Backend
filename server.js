dd5ee9bbb7829f73d2d43278e74f1553e1d89bdd
Yeniffer8a <jennyov19@hotmail.com>
2024-12-28 22:30:13 -0500

    Swagger created

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ import express from "express";
 import connectDB from "./config/database.js";
 import cors from "cors";
 import apiRouter from "./routes/api.router.js";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+
+const swaggerDocument = YAML.load("./openapi.yaml");
 
 const app = express();
 const PORT = process.env.APP_PORT || "3001";
@@ -12,6 +16,7 @@ connectDB();
 app.use(express.json());
 app.use(cors());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("", apiRouter);
 
 app.listen(PORT, () => {
